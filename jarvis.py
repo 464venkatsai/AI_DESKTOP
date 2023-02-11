778cba2179373b8a3ea700970f16e84813a42751
Yogeswarvenkatasai <112299999+464venkatsai@users.noreply.github.com>
2023-02-11 15:13:53 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -1,9 +1,14 @@
 # This module is used to deliver speech
 import pyttsx3
 import datetime
+import mysql.connector
 import speech_recognition as sr
 import wikipedia as wp
 import webbrowser as wb
+import os 
+import random
+from bs4 import BeautifulSoup
+import requests
 # This is the inbuilt API of the microsoft for taking the speech 
 engine=pyttsx3.init('sapi5')
 # Simply capturing the voices in the list 
@@ -37,10 +42,12 @@ def takeCommand():
         recorder.energy_threshold=430
         recorder.pause_threshold=1 # This is the attribute in the recognizer . This helps not to complete the whole phrase in 0.8 seconds which is default value
         audio = recorder.listen(source) # The sound captured by the source is stored in the audio
+
     try :
         print('Recognizing...')
         query = recorder.recognize_google(audio,language='en-in') # This function prints the result2 which is not required so i commented it out
         print(f'user said : {query}')
+        
     except Exception as e :
         # print(e)
         speak('Say again please...')
@@ -53,7 +60,6 @@ if __name__=='__main__':
     wish_me()
     while True:
         query=f'{takeCommand()}'
-        
         if 'wikipedia' in query:
             speak('searching wikipedia...')
             query=query.replace('wikipedia','')
@@ -61,31 +67,89 @@ if __name__=='__main__':
             print('According to wikipedia : ',result)
             speak('According to wikipedia :')
             speak(result)
-        if 'open' in query and 'youtube' in query:
-            wb.open('youtube.com')
-        if 'open' in query and 'google' in query:
-            wb.open('google.com')
-        if 'open' in query and 'stackoverflow' in query:
-            wb.open('stackoverflow.com')
-        if 'open' in query and 'spotify' in query:
-            wb.open('spotify.com')
+        if 'open' in query and 'youtube' in query : wb.open('youtube.com')
+
+        if 'open' in query and 'google' in query : wb.open('google.com')
+
+        if 'open' in query and 'stackoverflow' in query : wb.open('stackoverflow.com')
+
+        if 'open' in query and 'spotify' in query : wb.open('spotify.com')
+
         if 'quit' in query:
-            if 'you' and ('well' or 'good') in query:
-                speak('I am glad I helped you')
+            if 'you' and ('well' or 'good') in query : speak('I am glad I helped you')
             speak('Thanks for your time')
             break
+
         if 'want' and 'typing speed' in query:
             speak('Shall i open typing test.com in microsoft edge')
             ans=takeCommand()
-            if 'yes' in ans :
-                wb.open('typingtest.com')
-            else:
-                speak('what can i do for you ?')
+            if 'yes' in ans : wb.open('typingtest.com')
+
+            else : speak('what can i do for you ?')
+
         if 'want' and 'problem' in query:
             if 'coding' in query:
                 speak('continue your coding journey in codewars')
                 wb.open('codewars.com')
+
         if 'time' in query:
             current_time=datetime.datetime.now().strftime('%H:%M:%S')
             speak(f'current time is {current_time}')
 
+        if 'generate' and 'random' and 'numbers' in query:
+            # This is used to generate random values to the give times
+            print('the random numbers generated are : \n ',[random.randint(0,100) for i in range([i for i in query.split(' ') if i.isdigit()==True])])
+
+        if 'find' and 'folder' in  query:
+            print('The file will be checked in :',os.getcwd(),'as default else ')
+            path = input('Enter the path that you required :')
+            def display_folders(path):
+                # This contains  the folder that are present in the current working directory
+                folder = os.listdir(path)
+                try:
+                    for i in folder:
+                            if os.path.isfile(i)==True : pass
+                            else:
+                                    print(i)
+                                    # Again sending the path to same fucntion
+                                    display_folders(path+f'/{i}')
+                except:
+                    print('The path is wrong')
+            display_folders(path)
+
+        if 'create' and 'folder' in query :
+            path = input('Enter the name of the folder :')
+            # Creating a single directory 
+            os.mkdir(os.path.join(os.getcwd(),path))
+
+        if 'read' and 'folder' in query :
+            file = input('Enter the name of the folder :')
+            # Reading the file using with statement
+            with open('file.py','r') as file:
+                file_content = file.read()
+                print(file_content)
+
+        if 'delete' and 'folder':
+            file = input('Enter the name of the folder :')
+            # os.path.join(os.getcwd(),file) is a optimzed path 
+            os.remove(os.path.join(os.getcwd(),file))
+            print(f'The {file} is deleted successfully')
+
+
+        if 'database' in query:
+            import mysql.connector
+            # Establishing connection by using the mysql.connector.connect method 
+            # my user is root password is 1234
+            # Local host is host 
+            # database is mydb
+            connection = mysql.connector.connect(user='root',password='password', host='127.0.0.1', database='mydb')
+            # Making a cursor to operate
+            mycursor = connection.cursor()
+            # Executing the SQL commands using execute function
+            mycursor.execute('show database;')
+        
+        if 'HTML' and 'website':
+            # Importing BS4 module  to extract the HTML
+            # This is a direct one liner that take 6 line to write
+            print(BeautifulSoup(requests.get(input('Enter url of the website')).content,'html.parser').prettify())
+        
